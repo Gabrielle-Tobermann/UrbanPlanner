383af197729a0b5ca1b22a3fdb9ba090d5b31d5d
Gabrielle-Tobermann <gabrielle.tobermann@gmail.com>
2021-07-24 11:57:58 -0500

    Add project files.

---
+++ UrbanPlanner/Building.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UrbanPlanner
+{
+    class Building
+    {
+        public int Stories { get; set; }
+        public double Width { get; set; }
+        public double Depth { get; set; }
+        public double Volume
+        {
+            get
+            {
+                return Width * Depth * (3 * Stories);
+            }
+        }
+
+        public string _designer = "Gab";
+        public DateTime _dateConstructed;
+        public string _address;
+        public string _owner;
+
+        public Building(string address)
+        {
+            _address = address;
+        }
+
+        public void Construct()
+        {
+            _dateConstructed = DateTime.Now;
+        }
+
+        public void Purchase(string purchaser)
+        {
+            _owner = purchaser;
+        }
+    }
+}
---
+++ UrbanPlanner/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace UrbanPlanner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Building One = new Building("1234 1st Avenue");
+            One.Width = 10.5;
+            One.Depth = 20.5;
+            One.Stories = 6;
+
+            Building Two = new Building("1234 2nd Avenue");
+            One.Width = 15.5;
+            One.Depth = 25.5;
+            One.Stories = 8;
+
+            Building Three = new Building("1234 3rd Avenue");
+            One.Width = 14.5;
+            One.Depth = 29.5;
+            One.Stories = 15;
+
+            One.Construct();
+            Two.Construct();
+            Three.Construct();
+
+            One.Purchase("Lindsey");
+            Two.Purchase("Honey-Rae");
+            Three.Purchase("Tad");
+
+            Console.WriteLine($"{One._address} --------\nDesigned by {One._designer}\nConstructed on {One._dateConstructed}\n" +
+                $"Owned by {One._owner}\n{One.Volume} cubic meters of space");
+        }
+    }
+}
